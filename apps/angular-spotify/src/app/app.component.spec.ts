288f04ea1f38ff358f9c65b9555cc45151b1b71c
walidbouguima <walid.bouguima@gmail.com>
2022-10-17 10:31:23 +0200

    chore: linting update for the main component

--- apps/angular-spotify/src/app/app.component.spec.ts
+++ apps/angular-spotify/src/app/app.component.spec.ts
@@ -7,7 +7,6 @@ describe('AppComponent', () => {
       declarations: [AppComponent]
     }).compileComponents();
   });
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;

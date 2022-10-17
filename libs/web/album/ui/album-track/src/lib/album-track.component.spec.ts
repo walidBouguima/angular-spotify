b99add273de60938b1838e94577448b1523344ad
walidbouguima <walid.bouguima@gmail.com>
2022-10-17 18:22:42 +0200

    test(libs:Album): adding test suites for the DOM elements existence

--- libs/web/album/ui/album-track/src/lib/album-track.component.spec.ts
+++ libs/web/album/ui/album-track/src/lib/album-track.component.spec.ts
@@ -5,6 +5,8 @@ import { SvgIconsModule } from "@ngneat/svg-icon"
 import { TrackApiService } from "@angular-spotify/web/shared/data-access/spotify-api"
 import { APP_CONFIG } from "@angular-spotify/web/shared/app-config"
 import { HttpClientTestingModule} from "@angular/common/http/testing"
+import { DebugElement } from "@angular/core"
+import { By } from "@angular/platform-browser"
 describe("album track component test", ()=>{
     const dashboardIcon = {
         data: `<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m197.332031 170.667969h-160c-20.585937 0-37.332031-16.746094-37.332031-37.335938v-96c0-20.585937 16.746094-37.332031 37.332031-37.332031h160c20.589844 0 37.335938 16.746094 37.335938 37.332031v96c0 20.589844-16.746094 37.335938-37.335938 37.335938zm-160-138.667969c-2.941406 0-5.332031 2.390625-5.332031 5.332031v96c0 2.945313 2.390625 5.335938 5.332031 5.335938h160c2.945313 0 5.335938-2.390625 5.335938-5.335938v-96c0-2.941406-2.390625-5.332031-5.335938-5.332031zm0 0"/><path d="m197.332031 512h-160c-20.585937 0-37.332031-16.746094-37.332031-37.332031v-224c0-20.589844 16.746094-37.335938 37.332031-37.335938h160c20.589844 0 37.335938 16.746094 37.335938 37.335938v224c0 20.585937-16.746094 37.332031-37.335938 37.332031zm-160-266.667969c-2.941406 0-5.332031 2.390625-5.332031 5.335938v224c0 2.941406 2.390625 5.332031 5.332031 5.332031h160c2.945313 0 5.335938-2.390625 5.335938-5.332031v-224c0-2.945313-2.390625-5.335938-5.335938-5.335938zm0 0"/><path d="m474.667969 512h-160c-20.589844 0-37.335938-16.746094-37.335938-37.332031v-96c0-20.589844 16.746094-37.335938 37.335938-37.335938h160c20.585937 0 37.332031 16.746094 37.332031 37.335938v96c0 20.585937-16.746094 37.332031-37.332031 37.332031zm-160-138.667969c-2.945313 0-5.335938 2.390625-5.335938 5.335938v96c0 2.941406 2.390625 5.332031 5.335938 5.332031h160c2.941406 0 5.332031-2.390625 5.332031-5.332031v-96c0-2.945313-2.390625-5.335938-5.332031-5.335938zm0 0"/><path d="m474.667969 298.667969h-160c-20.589844 0-37.335938-16.746094-37.335938-37.335938v-224c0-20.585937 16.746094-37.332031 37.335938-37.332031h160c20.585937 0 37.332031 16.746094 37.332031 37.332031v224c0 20.589844-16.746094 37.335938-37.332031 37.335938zm-160-266.667969c-2.945313 0-5.335938 2.390625-5.335938 5.332031v224c0 2.945313 2.390625 5.335938 5.335938 5.335938h160c2.941406 0 5.332031-2.390625 5.332031-5.335938v-224c0-2.941406-2.390625-5.332031-5.332031-5.332031zm0 0"/></svg>`,
@@ -28,8 +30,28 @@ describe("album track component test", ()=>{
         }).compileComponents()
     })
     it('should be created successfully: ', ()=>{
-       const fixture = TestBed.createComponent(AlbumTrackComponent); 
-       const component = fixture.debugElement.componentInstance
+        const fixture = TestBed.createComponent(AlbumTrackComponent); 
+        const component = fixture.debugElement.componentInstance
         expect(component).toBeDefined()
+    })
+    it('should render the selector of as-media-table-row' , ()=> { 
+        const fixture = TestBed.createComponent(AlbumTrackComponent); 
+        const nativeElement: HTMLElement = fixture.nativeElement
+        expect(nativeElement.querySelector('as-media-table-row')).toBeDefined()
+    })
+    it('should render the selector of as-media-order ' , ()=> { 
+        const fixture = TestBed.createComponent(AlbumTrackComponent); 
+        const nativeElement: HTMLElement = fixture.nativeElement
+        expect(nativeElement.querySelector('as-media-order')).toBeDefined()
+    })
+    it('Should render the selctor of the as-track-main-info', ()=> { 
+        const fixture = TestBed.createComponent(AlbumTrackComponent) 
+        const nativeElement: HTMLElement = fixture.nativeElement
+        expect(nativeElement.querySelector('as-main-track-info')).toBeDefined()
+    })
+    it('Should render the div with the class name of text-description', ()=> { 
+        const fixture = TestBed.createComponent(AlbumTrackComponent) 
+        const nativeElement: DebugElement = fixture.debugElement
+        expect(nativeElement.query(By.css('text-description'))).toBeDefined()
     })
 })
